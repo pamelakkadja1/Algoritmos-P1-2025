9a0afaf62fd54f1e84530b7438abad4b1b21af30
Pamela Kadja <kadjapamelaoliveira@gmail.com>
2025-03-24 19:05:31 -0300

    Add files via upload

---
+++ Algor2.py
@@ -0,0 +1,6 @@
+senha= input ("Digite uma senha :")
+if(senha =="admin123" or senha == "123admin" or senha =="aid2m3i4n"):
+    print ("Acesso concluido com sucesso!!")
+else:
+    print("Acesso negado!")
+    
---
+++ Algoritmo.py
@@ -0,0 +1,5 @@
+num1 = int (input ("Digite o primeiro número:"))
+num2 = int (input ("Digite o segundo número:"))
+num3 = int (input("Digite o terceiro número :"))
+media = (num1 + num2 + num3)/3
+print (f"Média : {media}")
